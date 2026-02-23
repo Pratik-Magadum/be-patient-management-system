4fcb9555a04d360c270731e126c271b1b3de6bc4
simple-makes-easy <simplemakeseasy@gmail.com>
2026-02-23 20:03:43 +0530

    test setup

--- src/test/java/com/eyehospital/pms/patient/PatientServicekApplicationTests.java
+++ src/test/java/com/eyehospital/pms/patient/PatientServicekApplicationTests.java
@@ -5,9 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class PatientServicekApplicationTests {
 
 	@Test
