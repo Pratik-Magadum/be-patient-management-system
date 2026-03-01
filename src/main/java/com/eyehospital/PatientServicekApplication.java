9d75a1f8dcd506b6c3f0e8aa8c33ed8f29fc4063
simple-makes-easy <simplemakeseasy@gmail.com>
2026-03-01 17:11:14 +0530

    get config

--- src/main/java/com/eyehospital/PatientServicekApplication.java
+++ src/main/java/com/eyehospital/PatientServicekApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PatientServicekApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(PatientServicekApplication.class, args);
 	}
 
